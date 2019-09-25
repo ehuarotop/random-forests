c263ab8d6980bba35f646ae0f4b1576a0dd6f3d4
APNeto <penaneto@gmail.com>
2019-09-25 20:40:34 -0300

    Trying to iterate over data to build a bootstrap: some error with indentation is stopping the run

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ from decisionTree import decisionTree as dt
 
 def main():
 	data, data_desc = Utils.get_data_from_csv("datasets/dataset_191_wine.csv", ",")
+        #for linha in range(len(data.index)):
+        #    print(linha)
+        
+        # lista.append(data[randomint(1 , len(data.index))])
 	det = dt(data, data_desc)
 	root_node = det.generateDecisionTree()
 	det.renderDecisionTree(root_node)
